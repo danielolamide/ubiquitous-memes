1ec5a220cf4751b4450e520fa9df62d282ff369f
dannyolamide <dannyolamide@gmail.com>
2021-03-19 01:56:30 +0300

    project restructure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="flex flex-col w-full p-4">
+			<div className="flex justify-end">
+				<span>Test 1-2</span>
+			</div>
+		</div>
+	);
+}
+
+export default App;
--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<div>
-			<span>Test 1-2</span>
-		</div>
-	);
-}
-
-export default App;
