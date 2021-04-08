f9a8a5012dacd348a136978f8ef015733425d7c0
dannyolamide <dannyolamide@gmail.com>
2021-04-09 01:20:10 +0300

    identity permission

--- client/src/components/ui/MemeCard.js
+++ client/src/components/ui/MemeCard.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export default function MemeCard({ meme }) {
 	return (
-		<a href={meme.url} download={meme.tags[0]}>
+		<a
+			href={meme.url}
+			target="_blank"
+			rel="noreferrer"
+			alt={meme.tags[0]}
+			download={meme.tags[0]}
+		>
 			<img src={meme.url} />
 		</a>
 	);
